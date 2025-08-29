6db30873e31c395e6fb018e4acdc54847710d4aa
Javlonilyasov <167566038+Javlonilyasov@users.noreply.github.com>
2025-08-29 20:14:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import yt_dlp
 
-TOKEN = "7810423105:AAEhh_u4H2jeF58o6Pi6VNBgaccxqzm24og"
+TOKEN = "7810423105:AAESytg7NCZh6mxyUdfFKUxtjdvib0NTehs"
 
 # Random emoji reactionlar
 REACTIONS = ["👍", "🔥", "😂", "❤️", "👏", "😮"]
